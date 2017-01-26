a2d6e51b754d9333ed8ccae6b540261c49f5ad41
Fabian Kästner <kruemel01@users.noreply.github.com>
2017-01-27 00:25:13 +0100

    Commit

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,9 +3,12 @@ import * as Vue from "vue";
 import * as App from "./App.vue";
 import { parseStatement } from "./parser/parser";
 import InputStream from './parser/InputStream';
+import { StatementElement } from './parser/interfaces';
+import { MolecularFormula } from './chemical/chemical';
 
 new Vue({
     ...App,
 }).$mount("#app");
 
-console.log(parseStatement(new InputStream("N2 + 3H2 + 3.5O2 -- 2NO2 + 3H2O")));
+let statement: StatementElement[] = parseStatement(new InputStream("N2 + 3H2 + 3.5O2 -- 2NO2 + 3H2O"));
+console.log(statement);
